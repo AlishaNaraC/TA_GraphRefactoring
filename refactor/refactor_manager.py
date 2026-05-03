3e28515ddf6b728dc88f74b0ca8e72df2e5e07ba
AlishaNaraC <alishanara04@gmail.com>
2026-05-03 21:37:05 +0700

    delete db cloner module

--- refactor/refactor_manager.py
+++ refactor/refactor_manager.py
@@ -1,35 +1,17 @@
 from config import *
-
-from refactor.db_cloner import DBCloner
 from refactor.property_becoming_node import PropertyBecomingNode
 from refactor.edge_specification import EdgeSpecification
 
 class RefactorManager:
 
-    def __init__(self):
-        self.cloner = DBCloner(NEO4J_CONFIG)
-
-    def create_pbn_database(self):
-
-        print("Cloning baseline -> PBN")
-
-        self.cloner.clone_database(
-            BASELINE_DB,
-            PBN_DB
-        )
-
-    def create_es_database(self):
-
-        print("Cloning baseline -> ES")
-
-        self.cloner.clone_database(
-            BASELINE_DB,
-            ES_DB
-        )
-
     def run_pbn_refactor(self, properties):
 
-        refactor = PropertyBecomingNode(NEO4J_CONFIG, database=PBN_DB)
+        refactor = PropertyBecomingNode(
+            NEO4J_URI,
+            NEO4J_USER,
+            NEO4J_PASSWORD,
+            PBN_DB
+        )
 
         refactor.refactor_properties(properties)
 
@@ -37,7 +19,12 @@ class RefactorManager:
 
     def run_es_refactor(self, rel_type, properties):
 
-        refactor = EdgeSpecification(NEO4J_CONFIG, database=ES_DB)
+        refactor = EdgeSpecification(
+            NEO4J_URI,
+            NEO4J_USER,
+            NEO4J_PASSWORD,
+            ES_DB
+        )
 
         refactor.specialize_relationships(
             rel_type,
